5d80f1c31e81d7782e96b32a21a3560e4147e457
ososstech <66061846+ososstech@users.noreply.github.com>
2020-06-01 07:02:55 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,100 @@
+<?php
+session_start();
+?>
+<html lang="en">
+<head id="head">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no,user-scalable=no">
+	<link rel="shortcut icon" href="1.png">
+	<title>OsossTech</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link href="site/vendor/font-awesome/css/all.min.css" rel="stylesheet" type="text/css">
+	<link href="site/css/min/strapped.min.css" rel="stylesheet" type="text/css">
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+
+<body class="background" id="page-top" data-spy="scroll" data-target=".navbar" data-offset="50">
+	<div id="body">
+		<nav class="navbar fixed-top navbar-dark bg-dark navbar-expand-lg">
+			<a class="navbar-brand" href="#">
+  <img src="1.jpg" width="25" class="rounded">
+			OsossTech
+		</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="sign.php">SignIn | SignUp</a>
+    </div>
+  </div>
+</nav>
+		<div class="card mt-5">
+			<div class="cover-container d-flex w-99 h-99 p-3 mx-auto flex-column" style="margin-top: 60px;">
+				<main class="inner cover text-center">
+					<div class="text-center">
+  						<img src="1.jpg" class="rounded figure w-25">
+					</div>
+					<h1 class="cover-heading text-dark">OsossTech</h1>
+					<p class="lead text-muted">
+						Just Two Friends and a computer.
+					</p>
+					<img src="">
+					<ul class="list-group list-group-flush">
+						<a href="cydia://url/https://cydia.saurik.com/api/share#?source=https://ososstech.github.io/ososstech/" class="list-group-item list-group-item-action d-flex s justify-content-between" style="border-top-left-radius: 10px; border-top-right-radius: 10px;">
+							<strong class="text-primary">Open in Cydia</strong>
+							<span><i class="fas fa-angle-right text-muted"></i></span>
+						</a>
+						<a href="installer://add/https://ososstech.github.io/ososstech/" class="list-group-item list-group-item-action d-flex s justify-content-between">
+							<strong class="text-primary">Open in Installer</strong>
+							<span><i class="fas fa-angle-right text-muted"></i></span>
+						</a>
+						<a href="sileo://source/https://ososstech.github.io/ososstech/" class="list-group-item list-group-item-action d-flex s justify-content-between">
+							<strong class="text-primary">Open in Sileo</strong> 
+							<span><i class="fas fa-angle-right text-muted"></i></span> 
+						</a>
+						<a href="zbra://sources/add/https://ososstech.github.io/ososstech/" class="list-group-item list-group-item-action d-flex s justify-content-between" style="border-bottom-left-radius: 10px; border-bottom-right-radius: 10px;">
+							<strong class="text-primary">Open in Zebra</strong> 
+							<span><i class="fas fa-angle-right text-muted"></i></span>
+						</a>
+					</ul>
+				</main>
+				<hr>
+			</div>
+		</div>
+		<footer class="footer-text text-center" id="footer">
+			<div class="container">
+				<ul class="list-inline mb-5">
+					<li class="list-inline-item">
+						<br>
+				<br>
+						<a class="text-muted mr-3" href="https://twitter.com/ososstech">Twitter</a>
+					</li>
+					<li class="list-inline-item mr-3">|</li>
+					<li class="list-inline-item">
+						<a class="text-muted" href="https://github.com/ososstech/">GitHub</a>
+					</li>
+					<br>
+					<br>
+				<p class="text-muted small mb-0">Copyright © OsossTech 2019 - 2020</p>
+				<p class="text-muted small mb-0">
+					Made by: 
+					 <a class="text-muted" href="https://twitter.com/mohab_3dly">
+					 Mohab Adly
+					</a>
+					<a class="text-muted">
+					 &
+					</a>
+					<a class="text-muted" href="https://twitter.com/osamaibrahemm">
+					 Osama Ibraheem
+					</a>
+				</p>
+				</ul>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
